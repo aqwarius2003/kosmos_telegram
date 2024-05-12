f6dd63d6f7f13e2aa86bb0f0dffc6c06c04ae9ae
aqwarius2003 <00victorr@gmail.com>
2024-05-12 21:50:57 +0400

    Изменил имя переменной launch_id, сделал более плоским код

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -8,13 +8,13 @@ def fetch_spacex_last_launch(path_images_spicex, launch_id):
     url = f"https://api.spacexdata.com/v5/launches/{launch_id}"
     response = requests.get(url)
     response.raise_for_status()
-    response_data = response.json()
-    images = response_data['links']['flickr']['original']
-    if images:
-        for i, image_url in enumerate(images):
-            save_image(image_url, f'{path_images_spicex}/spacex_{i}.jpeg')
-    else:
+    last_launch_data = response.json()
+    images = last_launch_data['links']['flickr']['original']
+    if not images:
         print(f'Фотографии с запуска {launch_id} не публиковались')
+        return
+    for i, image_url in enumerate(images):
+        save_image(image_url, f'{path_images_spicex}/spacex_{i}.jpeg')
 
 
 if __name__ == '__main__':
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     parser.add_argument('-id', '--id_launch',
                         help='ID запуска, без указания скачает последние',
                         default='latest')
-    id_launch = parser.parse_args().id_launch
+    launch_id = parser.parse_args().id_launch
     path = 'images/'
     os.makedirs(path, exist_ok=True)
-    fetch_spacex_last_launch(path, id_launch)
+    fetch_spacex_last_launch(path, launch_id)
