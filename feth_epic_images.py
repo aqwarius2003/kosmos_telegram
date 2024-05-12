3fd1b361fa8fd65aad363315dcfcb617f8975dbe
aqwarius2003 <00victorr@gmail.com>
2024-05-12 21:40:48 +0400

    перенес из функции epic_download_images проверку указания даты EPIC фотографий пользователем

--- feth_epic_images.py
+++ feth_epic_images.py
@@ -6,15 +6,9 @@ from dotenv import load_dotenv
 import argparse
 
 
-def epic_download_images(token_nasa, path_images, date_photo=None):
+def epic_download_images(token_nasa, path_images, photo_date=None):
     payload = {'api_key': token_nasa}
-    if not date_photo:
-        url = 'https://api.nasa.gov/EPIC/api/natural/all'
-        response = requests.get(url, params=payload)
-        response.raise_for_status()
-        date_photo = response.json()[0]['date']
-
-    date_photos_epic = datetime.strptime(date_photo, '%Y-%m-%d').date()
+    date_photos_epic = datetime.strptime(photo_date, '%Y-%m-%d').date()
     url = f"https://api.nasa.gov/EPIC/api/natural/date/{str(date_photos_epic)}"
     response = requests.get(url, params=payload)
     response.raise_for_status()
@@ -29,6 +23,15 @@ def epic_download_images(token_nasa, path_images, date_photo=None):
         save_image(image_url, f'{path_images}{image_name}.png', payload)
 
 
+def get_latest_photo_date(api_key):
+    payload = {'api_key': api_key}
+    url = 'https://api.nasa.gov/EPIC/api/natural/all'
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+    photo_date = response.json()[0]['date']
+    return photo_date
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Скачивает EPIC фото с сайта NASA'
@@ -39,9 +42,14 @@ if __name__ == '__main__':
         help="Дата фотографий, формат %Y-%m-%d, '"
              "'без указания скачает последние",
     )
-    date_photos = parser.parse_args().date
+    photos_date = parser.parse_args().date
     load_dotenv()
     token_nasa = os.environ['API_TOKEN_NASA']
     path = 'images/'
+
+    if not photos_date:
+        photos_date = get_latest_photo_date(token_nasa)
+
     os.makedirs(path, exist_ok=True)
-    epic_download_images(token_nasa, path, date_photos)
+
+    epic_download_images(token_nasa, path, photos_date)
